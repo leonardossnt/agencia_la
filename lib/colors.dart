4dc650146b391605c84902c32836db5f05544933
Leonardo Santana <leonardossnt@gmail.com>
2022-09-22 23:44:10 -0400

    Create app color swatch

---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AgenciaLaColors {
+  static const primary = Color(0xff54aea9); // green
+  static const darkPrimary = Color(0xff29817c); // dark green
+  static const lightPrimary = Color(0xffaddcca); // tea green
+  static const onPrimary = Color(0xffffffff);
+  static const secondary = Color(0xff2b3c65); // dark blue
+  static const background = Color(0xffffffff); // white bg
+  static const onBackground = Color(0xff000000);
+  static const inputBackground = Color(0xfff5f5f5);
+  static const icons = Color(0xff344e4c);
+}
