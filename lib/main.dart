c94296f052f749c692a307c64b7f9becccd4e0e8
Leonardo Santana <leonardossnt@gmail.com>
2022-09-25 00:21:54 -0400

    Remove Firebase initialization from main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:agencia_la/app/app.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:agencia_la/firebase_options.dart';
-
 void main() {
-  initFirebase();
-
   runApp(const MyApp());
-}
-
-void initFirebase() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 }
