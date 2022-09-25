c5267d83a275073d69d0082fa5b7bf2316af2fc9
Leonardo Santana <leonardossnt@gmail.com>
2022-09-25 00:27:33 -0400

    Create Client's Main screen

---
+++ lib/screens/client_main_screen.dart
@@ -0,0 +1,50 @@
+import 'package:agencia_la/auth/auth.dart';
+import 'package:agencia_la/colors.dart';
+import 'package:agencia_la/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class ClientMainScreen extends StatelessWidget {
+  const ClientMainScreen({super.key});
+
+  void logout(BuildContext context) {
+    Auth.clearCurrentUser();
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+          builder: (context) => const LoginScreen()
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AgenciaLaColors.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Você está logado!"),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36),
+              child: ElevatedButton(
+                onPressed: () => logout(context),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AgenciaLaColors.primary,
+                  foregroundColor: AgenciaLaColors.onPrimary,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+                  minimumSize: const Size(double.infinity, 60),
+                ),
+                child: const Text(
+                  "Logout",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
