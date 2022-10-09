2209d0c5c46d1360f6345dc04d7b3e6daa65b4f9
Leonardo Santana <leonardossnt@gmail.com>
2022-10-08 22:39:58 -0400

    Add more data mocked

--- lib/model/mock.dart
+++ lib/model/mock.dart
@@ -2,6 +2,14 @@ import 'lanny.dart';
 import 'order.dart';
 
 class Mock {
-  static final lanny = Lanny(name: "Giovana", age: 25, phone: "(92) 99999-9999", picture: "assets/images/logo_navy.png");
-  static final order = Order(lanny: lanny, dateTime: "21/10 às 17:00", duration: "2h", address: "Rua Pará, 476 - Vieiralves, Cond. Smart Town Apt 2, Bl 8. CEP: 69099-000");
+  static final lannies = [
+    Lanny(name: "Giovana", age: 25, phone: "(92) 99999-9999", picture: "assets/images/logo_navy.png"),
+    Lanny(name: "Luiza", age: 21, phone: "(92) 99999-9999", picture: "assets/images/logo_teal.png"),
+  ];
+  static final orders = [
+    Order(lanny: lannies[0], date: "21/10", time: "17:00", duration: "2h", address: "Rua Pará, 476 - Vieiralves, Cond. Smart Town Apt 2, Bl 8. CEP: 69099-000"),
+    Order(lanny: lannies[1], date: "06/10", time: "19:00", duration: "3h", address: "Rua Pará, 476 - Vieiralves, Cond. Smart Town Apt 2, Bl 8. CEP: 69099-000"),
+    Order(lanny: lannies[1], date: "30/09", time: "17:00", duration: "4h", address: "Rua Pará, 476 - Vieiralves, Cond. Smart Town Apt 2, Bl 8. CEP: 69099-000"),
+    Order(lanny: lannies[0], date: "23/09", time: "08:00", duration: "4h", address: "Rua Pará, 476 - Vieiralves, Cond. Smart Town Apt 2, Bl 8. CEP: 69099-000"),
+  ];
 }
